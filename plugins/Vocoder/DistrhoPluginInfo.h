62ff17e27b6305f1eecca8a570d847b39d0cc28e
Stazed <stazed@manjo-mapson.com>
2023-12-10 20:10:16 -0600

    Rename voc >> Vocoder.

--- plugins/Vocoder/DistrhoPluginInfo.h
+++ plugins/Vocoder/DistrhoPluginInfo.h
@@ -1,9 +1,9 @@
 #ifndef DISTRHO_PLUGIN_INFO_H_INCLUDED
 #define DISTRHO_PLUGIN_INFO_H_INCLUDED
 
-#define DISTRHO_PLUGIN_NAME  "voc"
-#define DISTRHO_PLUGIN_URI   "https://Stazed@github.com/Stazed/voc"
-#define DISTRHO_PLUGIN_CLAP_ID "com.stazed.github.voc"
+#define DISTRHO_PLUGIN_NAME  "Vocoder"
+#define DISTRHO_PLUGIN_URI   "https://Stazed@github.com/Stazed/vocoder"
+#define DISTRHO_PLUGIN_CLAP_ID "com.stazed.github.vocoder"
 
 #define DISTRHO_PLUGIN_NUM_INPUTS   2
 #define DISTRHO_PLUGIN_NUM_OUTPUTS  1
